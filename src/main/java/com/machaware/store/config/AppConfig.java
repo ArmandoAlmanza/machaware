3e3a8804dccad11d9c7d54e06fd2f09480a9d001
Armando Almanza <armando.s.almanza@gmail.com>
2024-05-14 15:08:54 +0000

    make config to accept messages from propperties

---
+++ src/main/java/com/machaware/store/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.machaware.store.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:messages.properties")
+public class AppConfig {
+
+}
