91c496eccb7b6058d0fb775a6bb82f75b37066ce
Armando Almanza <armando.s.almanza@gmail.com>
2024-05-14 15:09:20 +0000

    add post Mapping and validate

--- src/main/java/com/machaware/store/controllers/UserController.java
+++ src/main/java/com/machaware/store/controllers/UserController.java
@@ -5,11 +5,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.machaware.store.models.User;
 import com.machaware.store.services.UserService;
+import com.machaware.store.utils.Validator;
+
+import jakarta.validation.Valid;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,9 +26,24 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
+	private Validator validator = new Validator();
+
 	@GetMapping()
 	public List<User> getAllUSers() {
 		return userService.getAllUSers();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getUserById(@PathVariable Long id) {
+		return userService.getUserId(id);
+	}
+
+	@PostMapping()
+	public ResponseEntity<?> createUser(@Valid @RequestBody User user, BindingResult result) {
+		if (result.hasErrors()) {
+			return validator.validation(result);
+		}
+		return userService.createUser(user);
 	}
 
 }
