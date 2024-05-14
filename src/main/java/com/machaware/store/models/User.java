22711a45176c74734a32c0775704f239168b38ae
Armando Almanza <armando.s.almanza@gmail.com>
2024-05-14 15:09:48 +0000

    add Defaul rol value and notations from validation

--- src/main/java/com/machaware/store/models/User.java
+++ src/main/java/com/machaware/store/models/User.java
@@ -6,6 +6,9 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,22 +21,30 @@ import lombok.Setter;
 @Entity
 @Table(name = "users")
 public class User {
+
+	@NotNull
 	@Id
 	private Long id;
 
+	@NotBlank
+	@Size(min = 3, max = 25)
 	@Column(name = "first_name")
 	private String firstName;
 
+	@NotBlank
+	@Size(min = 5, max = 30)
 	@Column(name = "last_name")
 	private String lastName;
 
+	@NotBlank
 	@Column(name = "email")
 	private String email;
 
+	@NotBlank
 	@Column(name = "password")
 	private String password;
 
 	@Column(name = "rol")
 	@Enumerated(EnumType.STRING)
-	private Rol rol;
+	private Rol rol = Rol.USER;
 }
