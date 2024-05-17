c4325b15f2b271a84ea02aa641cb98827929944e
Armando Almanza <armando.s.almanza@gmail.com>
2024-05-17 15:59:45 +0000

    add the filter chain config

--- src/main/java/com/machaware/store/security/SecurityConfig.java
+++ src/main/java/com/machaware/store/security/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.machaware.store.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +14,14 @@ public class SecurityConfig {
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.authorizeHttpRequests(
+				(auth) -> auth.requestMatchers("/api/users").permitAll().anyRequest().authenticated())
+				.csrf(csrf -> csrf.disable())
+				.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.build();
 	}
 }
